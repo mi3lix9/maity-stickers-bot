0fba5a6dbc0b71e1702569b11960a766fa7ab087
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-06-16 02:45:41 +0300

    make config async

--- mod.ts
+++ mod.ts
@@ -1,14 +1,14 @@
 import { webhookCallback } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
 // You might modify this to the correct way to import your `Bot` object.
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { configAsync } from "https://deno.land/x/dotenv/mod.ts";
 
 import { bot } from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
 serve({
-  ["/" + config().BOT_TOKEN]: async (req) => {
+  ["/" + (await configAsync()).BOT_TOKEN]: async (req) => {
     if (req.method == "POST") {
       try {
         return await handleUpdate(req);
