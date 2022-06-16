8912749091a08f1f0644d73e296502fa5fb558ab
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-06-16 19:48:08 +0300

    try download api

--- bot.ts
+++ bot.ts
@@ -11,6 +11,8 @@ import {
   conversations,
   createConversation,
 } from "https://deno.land/x/grammy_conversations@v0.6.2/mod.ts";
+import { download } from "https://deno.land/x/download/mod.ts";
+
 import {
   FileFlavor,
   hydrateFiles,
@@ -28,72 +30,73 @@ type MyContext = FileFlavor<Context> &
 
 type MyConversation = Conversation<MyContext>;
 
-// async function createStickerPack(conversation: MyConversation, ctx: MyContext) {
-//   let name: string;
-//   let title: string;
-//   let stickerPath: string;
-//   let emoji: string;
-//   await ctx.reply("Send a title for your pack");
-//   ctx = await conversation.waitFor(":text");
-//   title = ctx.message?.text!;
+async function createStickerPack(conversation: MyConversation, ctx: MyContext) {
+  let name: string;
+  let title: string;
+  let stickerPath: string;
+  let emoji: string;
+  await ctx.reply("Send a title for your pack");
+  ctx = await conversation.waitFor(":text");
+  title = ctx.message?.text!;
 
-//   await ctx.reply("Send a name for your pack");
-//   ctx = await conversation.waitFor(":text");
-//   name = ctx.message?.text!;
+  await ctx.reply("Send a name for your pack");
+  ctx = await conversation.waitFor(":text");
+  name = ctx.message?.text!;
 
-//   while (name.includes(" ")) {
-//     await ctx.reply("Name cannot contain spaces");
-//     await ctx.reply("Send a name for your pack");
-//     ctx = await conversation.waitFor(":text");
-//     name = ctx.message?.text!;
-//   }
+  while (name.includes(" ")) {
+    await ctx.reply("Name cannot contain spaces");
+    await ctx.reply("Send a name for your pack");
+    ctx = await conversation.waitFor(":text");
+    name = ctx.message?.text!;
+  }
 
-//   // const isSticker = !!(await bot.api.getStickerSet(name));
+  // const isSticker = !!(await bot.api.getStickerSet(name));
 
-//   // while (!isSticker) {
-//   //   await ctx.reply("Name is already taken");
-//   //   await ctx.reply("Send a name for your pack");
-//   //   ctx = await conversation.waitFor(":text");
-//   //   name = ctx.message?.text!;
-//   // }
+  // while (!isSticker) {
+  //   await ctx.reply("Name is already taken");
+  //   await ctx.reply("Send a name for your pack");
+  //   ctx = await conversation.waitFor(":text");
+  //   name = ctx.message?.text!;
+  // }
 
-//   const getBot = await bot.api.getMe();
-//   name += "_by_" + getBot.username;
+  const getBot = await bot.api.getMe();
+  name += "_by_" + getBot.username;
 
-//   await ctx.reply("Send a sticker");
-//   ctx = await conversation.waitFor([":sticker", ":photo"]);
+  await ctx.reply("Send a sticker");
+  ctx = await conversation.waitFor([":sticker", ":photo"]);
 
-//   const { width, height } = ctx.message?.photo?.[0]! || ctx.message?.sticker!;
-//   const aspectRatio = width / height;
-//   const newWidth = width >= height ? 512 : 512 / aspectRatio;
-//   const newHeight = height >= width ? 512 : 512 / aspectRatio;
+  const { width, height } = ctx.message?.photo?.[0]! || ctx.message?.sticker!;
+  const aspectRatio = width / height;
+  const newWidth = width >= height ? 512 : 512 / aspectRatio;
+  const newHeight = height >= width ? 512 : 512 / aspectRatio;
 
-//   const file = await ctx.getFile();
-//   stickerPath = await conversation.external(() =>
-//     file?.download("./tmp/" + file.file_id)
-//   );
-//   // stickerPath = sticker?.getUrl();
+  const file = await ctx.getFile();
 
-//   await ctx.reply("Send emojis for this sticker");
-//   ctx = await conversation.waitFor(":text");
-//   emoji = ctx.message?.text!;
+  // stickerPath = await conversation.external(() =>
+  //   file?.download("./tmp/" + file.file_id)
+  // );
+  // stickerPath = sticker?.getUrl();
 
-//   const newSticker = await resize(Deno.readFileSync(stickerPath), {
-//     width: newWidth,
-//     height: newHeight,
-//   });
+  await ctx.reply("Send emojis for this sticker");
+  ctx = await conversation.waitFor(":text");
+  emoji = ctx.message?.text!;
 
-//   const t = await ctx.api.createNewStickerSet(
-//     ctx.from!.id,
-//     name!,
-//     title!,
-//     emoji!,
-//     { png_sticker: new InputFile(newSticker) }
-//   );
+  const newSticker = await resize(Deno.readFileSync(file.getUrl()), {
+    width: newWidth,
+    height: newHeight,
+  });
 
-//   await ctx.reply(`Sticker pack created!: \n https://t.me/addstickers/${name}`);
-//   await Deno.remove(".tmp/" + file.file_id);
-// }
+  const t = await ctx.api.createNewStickerSet(
+    ctx.from!.id,
+    name!,
+    title!,
+    emoji!,
+    { png_sticker: new InputFile(newSticker) }
+  );
+
+  await ctx.reply(`Sticker pack created!: \n https://t.me/addstickers/${name}`);
+  // await Deno.remove(".tmp/" + file.file_id);
+}
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN") || config().BOT_TOKEN;
 
@@ -102,16 +105,37 @@ export const bot = new Bot<MyContext>(BOT_TOKEN);
 bot.api.config.use(hydrateFiles(bot.token));
 bot.use(session({ initial: () => ({}) }));
 bot.use(conversations());
-// bot.use(createConversation(createStickerPack));
+bot.use(createConversation(createStickerPack));
 
-// bot.command("newpack", (ctx) => ctx.conversation.enter("createStickerPack"));
+bot.command("newpack", (ctx) => ctx.conversation.enter("createStickerPack"));
 
 bot.command("start", (ctx) =>
   ctx.reply("This bot is not ready yet, please wait ❤️")
 );
 
+bot.command("fevoratefood", (ctx) => ctx.conversation.enter("fevorateFood"));
+
+bot.on([":sticker", ":photo"], async (ctx) => {
+  const { width, height } = ctx.message?.photo?.[0]! || ctx.message?.sticker!;
+  const aspectRatio = width / height;
+  const newWidth = width >= height ? 512 : 512 / aspectRatio;
+  const newHeight = height >= width ? 512 : 512 / aspectRatio;
+  const file = await ctx.getFile();
+
+  // const res = await fetch(file.getUrl());
+  const tz = await download(file.getUrl());
+  console.log(tz);
+
+  // const newSticker = await resize(body, {
+  //   width: newWidth,
+  //   height: newHeight,
+  // });
+
+  // await ctx.replyWithPhoto(new InputFile(newSticker));
+});
+
 bot.catch((error) => {
   error.ctx.reply("Something went wrong");
   console.error(error);
 });
-// bot.start();
+bot.start();
