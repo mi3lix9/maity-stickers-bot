48556cc658e3a845481d802a5456749c1e069146
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-06-17 13:33:04 +0300

    feats: resize image, add sticker to existing pack

--- mod.ts
+++ mod.ts
@@ -1,14 +1,14 @@
 import { webhookCallback } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
 // You might modify this to the correct way to import your `Bot` object.
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 
+import { BOT_TOKEN } from "./constants.ts";
 import { bot } from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
 serve({
-  ["/" + Deno.env.get("BOT_TOKEN") || config().BOT_TOKEN]: async (req) => {
+  ["/" + BOT_TOKEN]: async (req) => {
     if (req.method == "POST") {
       try {
         return await handleUpdate(req);
