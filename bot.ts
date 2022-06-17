f7c52e4b0322d62c8c3e559eb17bab7940fac443
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-06-17 13:22:55 +0300

    try remove conversations

--- bot.ts
+++ bot.ts
@@ -29,85 +29,84 @@ type MyContext = FileFlavor<Context> &
 
 type MyConversation = Conversation<MyContext>;
 
-async function createStickerPack(conversation: MyConversation, ctx: MyContext) {
-  await ctx.reply("Send a title for your pack");
-  ctx = await conversation.waitFor(":text");
-  const title = ctx.message?.text!;
+// async function createStickerPack(conversation: MyConversation, ctx: MyContext) {
+//   await ctx.reply("Send a title for your pack");
+//   ctx = await conversation.waitFor(":text");
+//   const title = ctx.message?.text!;
 
-  let name: string;
-  let isAvailable: boolean;
-  do {
-    await ctx.reply("Send a name for your pack");
-    ctx = await conversation.waitFor(":text");
-    name = ctx.message?.text!;
-    isAvailable = !!(await bot.api.getStickerSet(name));
-  } while (name.includes(" ") && !isAvailable);
+//   let name: string;
+//   let isAvailable: boolean;
+//   do {
+//     await ctx.reply("Send a name for your pack");
+//     ctx = await conversation.waitFor(":text");
+//     name = ctx.message?.text!;
+//     isAvailable = !!(await bot.api.getStickerSet(name));
+//   } while (name.includes(" ") && !isAvailable);
 
-  const getBot = await bot.api.getMe();
-  name += "_by_" + getBot.username;
+//   const getBot = await bot.api.getMe();
+//   name += "_by_" + getBot.username;
 
-  const { emojis, sticker } = await getSticker(ctx, conversation);
+//   const { emojis, sticker } = await getSticker(ctx, conversation);
 
-  await ctx.api.createNewStickerSet(ctx.from!.id, name!, title!, emojis!, {
-    png_sticker: sticker!,
-  });
+//   await ctx.api.createNewStickerSet(ctx.from!.id, name!, title!, emojis!, {
+//     png_sticker: sticker!,
+//   });
 
-  await ctx.reply(
-    `Sticker pack created!: \n https://t.me/addstickers/${name} \n\n you can send more stickers, or send /done to stop`
-  );
+//   await ctx.reply(
+//     `Sticker pack created!: \n https://t.me/addstickers/${name} \n\n you can send more stickers, or send /done to stop`
+//   );
 
-  ctx = await conversation.waitFor(":text");
-  if (ctx.message?.text == "/done") {
-    return await ctx.reply("Sticker pack created!");
-  }
+//   ctx = await conversation.waitFor(":text");
+//   if (ctx.message?.text == "/done") {
+//     return await ctx.reply("Sticker pack created!");
+//   }
 
-  await ctx.conversation.enter("addSticker");
-}
+//   await ctx.conversation.enter("addSticker");
+// }
 
-async function addSticker(conversation: MyConversation, ctx: MyContext) {
-  await ctx.reply("Send a sticker from your pack that you want to add");
-  ctx = await conversation.waitFor(":sticker");
-  const name = ctx.message?.text!;
+// async function addSticker(conversation: MyConversation, ctx: MyContext) {
+//   await ctx.reply("Send a sticker from your pack that you want to add");
+//   ctx = await conversation.waitFor(":sticker");
+//   const name = ctx.message?.text!;
 
-  do {
-    await ctx.reply("Now, send a sticker or a photo to add into your pack");
-    ctx = await conversation.waitFor([":sticker", ":photo"]);
+//   do {
+//     await ctx.reply("Now, send a sticker or a photo to add into your pack");
+//     ctx = await conversation.waitFor([":sticker", ":photo"]);
 
-    const { sticker, emojis } = await getSticker(ctx, conversation);
-    await ctx.api.addStickerToSet(ctx.from!.id, name, emojis, {
-      png_sticker: sticker,
-    });
-    await ctx.reply("Sticker added!, send another sticker or /done to stop");
-  } while (ctx.message?.text != "/done");
-}
+//     const { sticker, emojis } = await getSticker(ctx, conversation);
+//     await ctx.api.addStickerToSet(ctx.from!.id, name, emojis, {
+//       png_sticker: sticker,
+//     });
+//     await ctx.reply("Sticker added!, send another sticker or /done to stop");
+//   } while (ctx.message?.text != "/done");
+// }
 
 export const bot = new Bot<MyContext>(BOT_TOKEN);
 
 bot.api.config.use(hydrateFiles(bot.token));
 bot.use(session({ initial: () => ({}) }));
 bot.use(conversations());
-bot.use(createConversation(createStickerPack));
-bot.use(createConversation(addSticker));
+// bot.use(createConversation(createStickerPack));
+// bot.use(createConversation(addSticker));
 
-bot.command("newpack", (ctx) => ctx.conversation.enter("createStickerPack"));
-bot.command("addsticker", (ctx) => ctx.conversation.enter("addSticker"));
+// bot.command("newpack", (ctx) => ctx.conversation.enter("createStickerPack"));
+// bot.command("addsticker", (ctx) => ctx.conversation.enter("addSticker"));
 
-bot.command("start", async (ctx) => {
-  await ctx.api.setMyCommands([
-    { command: "newpack", description: "Create a new sticker pack" },
-    { command: "addsticker", description: "Add sticker to your existing pack" },
-  ]);
-
-  await ctx.reply(
+bot.command("start", (ctx) =>
+  ctx.reply(
     "Hello! I am a bot that helps you create sticker packs easily, please enter /newpack to start new pack or /addSticker to add sticker to your existing pack \n\n Please notice that, this sticker is still under development, so you might get unexpected results. Please report any bugs to @mi3lix9!"
-  );
-});
+  )
+);
 
+bot.api.setMyCommands([
+  { command: "newpack", description: "Create a new sticker pack" },
+  { command: "addsticker", description: "Add sticker to your existing pack" },
+]);
 // bot.command("myid", (ctx) => ctx.reply(ctx.from!.id.toString()));
 
 bot.catch(async (error) => {
   await error.ctx.reply("Something went wrong");
-  // error.ctx.api.sendMessage(BOT_OWNER_ID, JSON.stringify({ error }, null, 2));
+  error.ctx.api.sendMessage(BOT_OWNER_ID, JSON.stringify({ error }, null, 2));
   console.error(error);
 });
 
--- mod.ts
+++ mod.ts
@@ -2,8 +2,8 @@ import { webhookCallback } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
 // You might modify this to the correct way to import your `Bot` object.
 
-import { bot } from "./bot.ts";
 import { BOT_TOKEN } from "./constants.ts";
+import { bot } from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
