fdb89d3205db021b1b382242fd5fa35b4c8a37e3
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-06-17 13:01:11 +0300

    feats: resize image, add sticker to existing pack

--- bot.ts
+++ bot.ts
@@ -11,14 +11,13 @@ import {
   conversations,
   createConversation,
 } from "https://deno.land/x/grammy_conversations@v0.6.2/mod.ts";
-import { download } from "https://deno.land/x/download/mod.ts";
 
 import {
   FileFlavor,
   hydrateFiles,
 } from "https://deno.land/x/grammy_files@v1.0.4/mod.ts";
-import { resize } from "https://deno.land/x/deno_image/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { BOT_OWNER_ID, BOT_TOKEN } from "./constants.ts";
+import { resizeImage } from "./resizeImage.ts";
 
 interface SessionData {
   sets: string[];
@@ -31,74 +30,56 @@ type MyContext = FileFlavor<Context> &
 type MyConversation = Conversation<MyContext>;
 
 async function createStickerPack(conversation: MyConversation, ctx: MyContext) {
-  let name: string;
-  let title: string;
-  let stickerPath: string;
-  let emoji: string;
   await ctx.reply("Send a title for your pack");
   ctx = await conversation.waitFor(":text");
-  title = ctx.message?.text!;
+  const title = ctx.message?.text!;
 
-  await ctx.reply("Send a name for your pack");
-  ctx = await conversation.waitFor(":text");
-  name = ctx.message?.text!;
-
-  while (name.includes(" ")) {
-    await ctx.reply("Name cannot contain spaces");
+  let name: string;
+  let isAvailable: boolean;
+  do {
     await ctx.reply("Send a name for your pack");
     ctx = await conversation.waitFor(":text");
     name = ctx.message?.text!;
-  }
-
-  // const isSticker = !!(await bot.api.getStickerSet(name));
-
-  // while (!isSticker) {
-  //   await ctx.reply("Name is already taken");
-  //   await ctx.reply("Send a name for your pack");
-  //   ctx = await conversation.waitFor(":text");
-  //   name = ctx.message?.text!;
-  // }
+    isAvailable = !!(await bot.api.getStickerSet(name));
+  } while (name.includes(" ") && !isAvailable);
 
   const getBot = await bot.api.getMe();
   name += "_by_" + getBot.username;
 
-  await ctx.reply("Send a sticker");
-  ctx = await conversation.waitFor([":sticker", ":photo"]);
+  const { emojis, sticker } = await getSticker(ctx, conversation);
 
-  const { width, height } = ctx.message?.photo?.[0]! || ctx.message?.sticker!;
-  const aspectRatio = width / height;
-  const newWidth = width >= height ? 512 : 512 / aspectRatio;
-  const newHeight = height >= width ? 512 : 512 / aspectRatio;
-
-  const file = await ctx.getFile();
-
-  // stickerPath = await conversation.external(() =>
-  //   file?.download("./tmp/" + file.file_id)
-  // );
-  // stickerPath = sticker?.getUrl();
-
-  await ctx.reply("Send emojis for this sticker");
-  ctx = await conversation.waitFor(":text");
-  emoji = ctx.message?.text!;
-
-  const newSticker = await resize(Deno.readFileSync(file.getUrl()), {
-    width: newWidth,
-    height: newHeight,
+  await ctx.api.createNewStickerSet(ctx.from!.id, name!, title!, emojis!, {
+    png_sticker: sticker!,
   });
 
-  const t = await ctx.api.createNewStickerSet(
-    ctx.from!.id,
-    name!,
-    title!,
-    emoji!,
-    { png_sticker: new InputFile(newSticker) }
+  await ctx.reply(
+    `Sticker pack created!: \n https://t.me/addstickers/${name} \n\n you can send more stickers, or send /done to stop`
   );
 
-  await ctx.reply(`Sticker pack created!: \n https://t.me/addstickers/${name}`);
-  // await Deno.remove(".tmp/" + file.file_id);
+  ctx = await conversation.waitFor(":text");
+  if (ctx.message?.text == "/done") {
+    return await ctx.reply("Sticker pack created!");
+  }
+
+  await ctx.conversation.enter("addSticker");
 }
 
-const BOT_TOKEN = Deno.env.get("BOT_TOKEN") || config().BOT_TOKEN;
+async function addSticker(conversation: MyConversation, ctx: MyContext) {
+  await ctx.reply("Send a sticker from your pack that you want to add");
+  ctx = await conversation.waitFor(":sticker");
+  const name = ctx.message?.text!;
+
+  do {
+    await ctx.reply("Now, send a sticker or a photo to add into your pack");
+    ctx = await conversation.waitFor([":sticker", ":photo"]);
+
+    const { sticker, emojis } = await getSticker(ctx, conversation);
+    await ctx.api.addStickerToSet(ctx.from!.id, name, emojis, {
+      png_sticker: sticker,
+    });
+    await ctx.reply("Sticker added!, send another sticker or /done to stop");
+  } while (ctx.message?.text != "/done");
+}
 
 export const bot = new Bot<MyContext>(BOT_TOKEN);
 
@@ -106,36 +87,47 @@ bot.api.config.use(hydrateFiles(bot.token));
 bot.use(session({ initial: () => ({}) }));
 bot.use(conversations());
 bot.use(createConversation(createStickerPack));
+bot.use(createConversation(addSticker));
 
 bot.command("newpack", (ctx) => ctx.conversation.enter("createStickerPack"));
+bot.command("addsticker", (ctx) => ctx.conversation.enter("addSticker"));
 
 bot.command("start", (ctx) =>
-  ctx.reply("This bot is not ready yet, please wait ❤️")
+  ctx.reply(
+    "Hello! I am a bot that helps you create sticker packs easily, please enter /newpack to start new pack or /addSticker to add sticker to your existing pack \n\n Please notice that, this sticker is still under development, so you might get unexpected results. Please report any bugs to @mi3lix9!"
+  )
 );
 
-bot.command("fevoratefood", (ctx) => ctx.conversation.enter("fevorateFood"));
+bot.api.setMyCommands([
+  { command: "newpack", description: "Create a new sticker pack" },
+  { command: "addsticker", description: "Add sticker to your existing pack" },
+]);
+// bot.command("myid", (ctx) => ctx.reply(ctx.from!.id.toString()));
 
-bot.on([":sticker", ":photo"], async (ctx) => {
-  const { width, height } = ctx.message?.photo?.[0]! || ctx.message?.sticker!;
-  const aspectRatio = width / height;
-  const newWidth = width >= height ? 512 : 512 / aspectRatio;
-  const newHeight = height >= width ? 512 : 512 / aspectRatio;
-  const file = await ctx.getFile();
-
-  // const res = await fetch(file.getUrl());
-  const tz = await download(file.getUrl());
-  console.log(tz);
-
-  // const newSticker = await resize(body, {
-  //   width: newWidth,
-  //   height: newHeight,
-  // });
-
-  // await ctx.replyWithPhoto(new InputFile(newSticker));
-});
-
-bot.catch((error) => {
-  error.ctx.reply("Something went wrong");
+bot.catch(async (error) => {
+  await error.ctx.reply("Something went wrong");
+  error.ctx.api.sendMessage(BOT_OWNER_ID, JSON.stringify({ error }, null, 2));
   console.error(error);
 });
-bot.start();
+// bot.start();
+
+async function getSticker(ctx: MyContext, conversation: MyConversation) {
+  let sticker: string | InputFile;
+  let emojis = "";
+
+  await ctx.reply("Send a sticker");
+  ctx = await conversation.waitFor([":sticker", ":photo"]);
+  if (ctx.message?.sticker) {
+    sticker = ctx.message?.sticker.file_id;
+  } else if (ctx.message?.photo) {
+    const { width, height } = ctx.message?.photo?.[0]!;
+    const file = await ctx.getFile();
+    sticker = await resizeImage(file.getUrl(), width, height);
+  }
+
+  await ctx.reply("Send emojis for this sticker");
+  ctx = await conversation.waitFor(":text");
+  emojis = ctx.message?.text!;
+
+  return { sticker: sticker!, emojis };
+}
---
+++ resizeImage.ts
@@ -0,0 +1,18 @@
+import { resize } from "https://deno.land/x/deno_image@0.0.4/index.ts";
+import { InputFile } from "https://deno.land/x/grammy@v1.8.3/platform.deno.ts";
+
+export async function resizeImage(url: string, width: number, height: number) {
+  const aspectRatio = width / height;
+  const newWidth = width >= height ? 512 : 512 / aspectRatio;
+  const newHeight = height >= width ? 512 : 512 / aspectRatio;
+
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+  const uint = new Uint8Array(buffer);
+
+  const newSticker = await resize(uint, {
+    width: newWidth,
+    height: newHeight,
+  });
+  return new InputFile(newSticker);
+}
