8500914b4be538ee066798e22f5d112c0277b853
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-06-23 05:19:57 +0300

    Change file strusture Prepare for v0.2

--- types.ts
+++ types.ts
@@ -5,7 +5,19 @@ import type {
 } from "https://deno.land/x/grammy_conversations/mod.ts";
 import type { FileFlavor } from "https://deno.land/x/grammy_files/mod.ts";
 
-interface SessionData {}
+export interface SessionData {
+  /**
+   * set of sticker sets' names
+   */
+  sets: Set<string>;
+  /**
+   * fast mode enable users to add stickers automatically to special packs
+   * without the need to add title,name, or emojies to the set.
+   *
+   * Currently doesn't do anything.
+   */
+  fastMode: boolean;
+}
 
 export type MyContext = FileFlavor<Context> &
   SessionFlavor<SessionData> &
