f7cc80d90e8f91f19f460ff63aac9e7cf98e2f0e
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-08-04 01:22:19 +0300

    change error boundry placement

--- src/bot.ts
+++ src/bot.ts
@@ -37,17 +37,16 @@ export function initBot(token: string, storage?: StorageAdapter<SessionData>) {
     })
   );
   _bot.use(conversations());
+  _bot.use(bot);
   _bot.errorBoundary(async ({ ctx, error }) => {
-    console.log(error);
+    console.log({ error });
 
     if (await ctx.conversation.active()) {
       await ctx.conversation.exit();
     }
 
-    await ctx.reply("Something went wrong, please try again later.");
+    return await ctx.reply("Something went wrong, please try again later.");
   });
-  _bot.use(bot);
-
   _bot.catch(async (error) => {
     await error.ctx.reply(error.message);
     console.log(error.message);
